86bb982a971c876b10f5a23acf047ce59b2f4d0d
Diego-Alejo <bstivenacetobon888@gmail.com>
2025-09-15 16:44:48 -0500

    Add POST and GET

---
+++ Employeewhite/Employee.Backend/Controllers/EmployeesController.cs
@@ -0,0 +1,35 @@
+﻿using Employee.Backend.Data;
+using Employee.shared.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Employee.Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public EmployeesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> GetAsync()
+        {
+            return Ok(await _context.Employees.ToListAsync());
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> PostAsync(EmployeeBD employee) 
+        {
+            _context.Employees.Add(employee);
+            await _context.SaveChangesAsync();
+            return Ok(employee);
+        }
+    }
+}
