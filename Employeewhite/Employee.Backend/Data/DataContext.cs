a9d28508a0fce3683bbe66bd8dc3b7f45cb6ef51
Juanes <legendaryjeoa@gmail.com>
2025-09-15 00:34:23 -0500

    Add all of Employee

---
+++ Employeewhite/Employee.Backend/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Employee.shared.Entities;
+
+namespace Employee.Backend.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<EmployeeBD> Employees => Set<EmployeeBD>();
+    }
+}
---
+++ Employeewhite/Employee.shared/Entities/EmployeeBD.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee.shared.Entities
+{
+    public class EmployeeBD
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(30)]
+        public string FirstName { get; set; } = null!;
+
+        [Required, MaxLength(30)]
+        public string LastName { get; set; } = null!;
+
+        public bool IsActive { get; set; }
+
+        public DateTime HireDate { get; set; }
+
+        [Required, Range(1000000, double.MaxValue, ErrorMessage = "Salary must be at least 1,000,000.")]
+        public decimal Salary { get; set; }
+    }
+}
